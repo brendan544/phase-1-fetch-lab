039763d36870eb6a83d7b0b87841426685058bfd
brendan544 <brendan.gwer@student.moringaschool.com>
2024-07-15 15:24:26 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
-}
+  const apiUrl= 'https://anapioficeandfire.com/api/books';
 
+   // Fetch data from API
+   fetch(apiUrl)
+   .then(response => {
+       if (!response.ok) {
+           throw new Error(`HTTP error! Status: ${response.status}`);
+       }
+       return response.json(); // Parse the JSON from the response
+   })
+}
+.then(data => {
+  // Call renderBooks function with the JSON data
+  renderBooks(data);
+})
+.catch(error => {
+  console.error('Error fetching data:', error);
+});
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
